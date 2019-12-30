c75b8daaba974a5226f641de052e499fb0083c24
Sina Saidinejad <sinasn1996@gmail.com>
2019-12-30 17:41:58 +0330

    token generate added

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,33 +1,3 @@
-const express = require("express");
-const cors = require("cors");
-const server = require("./configs/server");
-const fs = require("fs");
-const jwt = require("jsonwebtoken");
-
-var app = express();
-
-app.use(cors());
-
-app.get("/", (req, res) => res.send("Hello World!"));
-
-// let's first add a /secret api endpoint that we will be protecting
-app.get("/secret", isAuthenticated, (req, res) => {
-  res.json({ message: "THIS IS SUPER SECRET, DO NOT SHARE!" });
-});
-
-// and a /readme endpoint which will be open for the world to see
-app.get("/readme", (req, res) => {
-  res.json({ message: "This is open to the world!" });
-});
-
-app.get("/jwt", (req, res) => {
-  let privateKey = fs.readFileSync("./data/private.pem", "utf8");
-  let token = jwt.sign({ body: "stuff" }, "MySuperSecretPassPhrase", {
-    algorithm: "HS256"
-  });
-  res.send(token);
-});
-
 function isAuthenticated(req, res, next) {
   if (typeof req.headers.authorization !== "undefined") {
     // retrieve the authorization header and parse out the
@@ -54,7 +24,3 @@ function isAuthenticated(req, res, next) {
     throw new Error("Not Authorized");
   }
 }
-
-app.listen(server.port, function() {
-  console.log(`Listening on port ${server.port}`);
-});
