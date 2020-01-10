3d9f44d895fa62c7311f4056e5b61ab608798aea
SinaSn <sinasn1996@gmail.com>
2020-01-11 01:31:24 +0330

    verify completed.

--- controllers/payment.js
+++ controllers/payment.js
@@ -1,4 +1,6 @@
 const Book = require("../models/book.model");
+const Cart = require("../models/cart.model");
+const Payment = require("../models/payment.model");
 var ZarinpalCheckout = require("../utils/zarinpal");
 
 async function payment({ userId, bookIds, discount, description, email }) {
@@ -16,15 +18,17 @@ async function payment({ userId, bookIds, discount, description, email }) {
     _discount = discount;
   }
 
+  let books = [];
+
   let bookArr = bookIds.split("/");
   for (let i = 0; i < bookArr.length; i++) {
     amount += await Book.findById(bookArr[i])
       .exec()
       .then(function(book) {
+        books.push(book);
         return book.price;
       });
   }
-
   amount -= amount * _discount;
 
   var zarinpal = ZarinpalCheckout.create(
@@ -39,7 +43,27 @@ async function payment({ userId, bookIds, discount, description, email }) {
       Description: description,
       Email: email
     })
-    .then(function(res) {
+    .then(async function(res) {
+      var newCart = new Cart({
+        userId: userId,
+        books: books,
+        price: amount,
+        count: bookArr.length,
+        dateCreated: Date.now()
+      });
+
+      var newPayment = new Payment({
+        user: userId,
+        cart: newCart,
+        price: amount,
+        succeeded: false,
+        refNumber: res.authority,
+        dateCreated: Date.now(),
+        reservedUntil: Date.now() + 15 * 60 * 1000
+      });
+
+      await newPayment.save();
+
       return res;
     })
     .catch(function(err) {
@@ -55,6 +79,10 @@ async function payment({ userId, bookIds, discount, description, email }) {
 
 async function verify({ authority, status }) {
   if (status === "OK") {
+    let payment = await Payment.findOne({ refNumber: authority }).exec();
+    payment.succeeded = true;
+    await payment.save();
+
     return {
       status: true,
       message: "عملیات با موفقیت انجام شد",
--- models/payment.model.js
+++ models/payment.model.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const Cart = require("../models/cart.model");
+
 const paymentSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: "users" },
-  cart: { type: Schema.Types.ObjectId, ref: "carts" },
+  cart: { type: Cart.schema },
   discount: { type: Schema.Types.ObjectId, ref: "discounts" },
   price: Number,
   succeeded: Boolean,
